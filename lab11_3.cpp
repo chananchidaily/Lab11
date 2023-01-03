8f1423d4f22f3760f2d194f3303df21c06345b22
tonkhaooo <chananchida_tha@cmu.ac.th>
2023-01-03 16:10:47 +0700

    hhhjjjj

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    
+    int N = 0;
+    double sum = 0,sum1 = 0 ,M;
+    float SD;
+    string score;
+
+    ifstream x;
+    x.open("score.txt");
+    while(getline(x,score)){
+        sum += atof(score.c_str());
+        sum1 += pow(atof(score.c_str()),2);
+        N++;
+    }
+    M = sum / N ;
+
+    SD = sqrt((sum1/N)-(M*M));
+
+    x.close();
+
+    cout << "Number of data = "<< N << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< M << endl;
+    cout << "Standard deviation = "<< SD;
+}
